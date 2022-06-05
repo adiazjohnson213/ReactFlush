ffbbe0ebb6acbca7355c48b7cf642da4f2cfa9ad
Arthur Diaz <mrarthur_213@outlook.com>
2022-06-04 19:24:18 -0500

    Flux inclusion

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,20 @@ import CoursesPage from "./CoursesPage";
 import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import ManageCoursePage from "./ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
     return (
         <div className="container-fuild">
+            <ToastContainer autoClose={3000} hideProgressBar />
             <Header/>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/courses" element={<CoursesPage/>}/>
                 <Route path="/about" element={<AboutPage/>}/>
                 <Route path="/course/:slug" element={<ManageCoursePage/>}/>
+                <Route path="/course" element={<ManageCoursePage/>}/>
                 <Route path="/*"element={<NotFoundPage/>}/>
             </Routes>
         </div>
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,71 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import CourseForm from "./CourseForm";
+import courseStore from "../stores/courseStore";
+import { toast } from "react-toastify"
+import * as courseActions from "../actions/courseActions";
 
-const ManageCoursePage = prop => {
-    const params = useParams();
+const ManageCoursePage = props => {
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState({});
+    const { slug } = useParams();
+    const [courses, setCourses] = useState(courseStore.getCourses());
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    });
+
+    useEffect(() => {
+        courseStore.addChangeListener(onChange);
+        if(courses.length === 0){
+            courseActions.loadCourses();
+        } else if (slug) {
+            setCourse(courseStore.getCourseBySlug(slug));
+        }
+        return () => courseStore.removeChangeListener(onChange);
+    }, [courses.length, slug]);
+
+    function onChange() {
+        setCourses(courseStore.getCourses());
+    }
+
+    const handleChange = event => {
+        setCourse({...course, [event.target.name]: event.target.value});
+    }
+
+    function formIsValid(){
+        const _errors = {};
+
+        if(!course.title) _errors.title = "Title is required";
+        if(!course.authorId) _errors.authorId = "Author ID is required";
+        if(!course.category) _errors.category = "Category is required";
+
+        setErrors(_errors);
+
+        return Object.keys(_errors).length === 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if(!formIsValid()) return;
+        courseActions.saveCourse(course).then(() => {
+            toast.success("Course saved");
+            navigate("/courses");
+        })
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
-            {params.slug}
+            <CourseForm 
+                errors={errors}
+                course={course}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
         </>
     );
 };
